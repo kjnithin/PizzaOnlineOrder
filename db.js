232f56da83f6b3e283f276826bc1d25c4634ada6
Nithin Jayappa <kjnithin89@gmail.com>
2017-08-28 01:15:35 -0400

    changed to production env

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.set('debug', true);
-mongoose.connect(process.env.DATABASE_DEV);
+mongoose.connect(process.env.DATABASE_PROD);
 mongoose.Promise = global.Promise;
 mongoose.connection.on('error', function(err) {
     console.log(err);
